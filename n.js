6feeb5e7acb59d1e039262c96624870204b9a348
gm6njbkbv4-bit <gm6njbkbv4@privaterelay.appleid.com>
2026-01-10 19:22:15 +0800

    Create n.js

---
+++ n.js
@@ -0,0 +1,20 @@
+window.onload = (ev) => {
+    // @ts-ignore
+    if (!window.mLoaded) {
+    // @ts-ignore    
+        window.mLoaded = true
+        main()
+    }
+}
+
+function main() {
+    const elements = document.getElementsByClassName('post-card')
+    for (const element of elements) {
+        const pHeader = element.getElementsByClassName('post-header')[0]
+        const pAuthorInfo = pHeader.getElementsByClassName('author-info')[0]
+        const pAuthorName = pAuthorInfo.children[0]
+        if (pAuthorName && (pAuthorName.innerHTML.includes('网友-28')) {
+            setTimeout(() => {element.remove()}, 20)
+        }
+    }
+}
